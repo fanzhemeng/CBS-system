58828ab96d43b7d37f4f28d499addbb3b6f8754f
fanzhemeng <f25meng@uwaterloo.ca>
2017-03-13 12:37:19 -0400

    rpcExecute

--- server_lib.cc
+++ server_lib.cc
@@ -62,3 +62,56 @@ int rpcRegister(char* name, int* argTypes, skeleton f) {
     return 0;
 }
 
+int rpcExecute(void) {
+	while (1) {
+		// update buffers to receive messages
+		selection(rec_sockfd);
+		// fetch the message in the read buffer
+		std::pair <int, std::string> request = respond();
+		if (request.first == -1) {
+			// err ?
+			return -1;
+		}
+
+		// get the actual message
+		std::string msg = request.second;
+		if (msg.length() < 7) {continue;} // invalid message
+
+		if (msg.substr(0, 7) == "EXECUTE") {
+			msg = msg.substr(7);
+			std::string fname = decode_fname(msg);
+			msg = msg.substr(sizeof(size_t) + fname.length());
+			std::pair<size_t, int*> a = decode_argtypes(msg);
+			int *argt = a.second;
+			msg = msg.substr(sizeof(size_t) + a.first * sizeof(int*));
+			void **args = decode_args(argt, msg);
+
+			// find and call skeleton function
+			skeleton f = funcs.at(std::make_pair(fname, argt));
+			int result = f(argt, args);
+
+			// now send result back to client
+			std::string enc_type, ret_msg;
+			if (result == 0) {
+				enc_type = std::string("") + (char)EXECUTE_SUCCESS;
+				std::string enc_fname = encode_fname(fname);
+				std::string enc_argt = encode_argtypes(argt);
+				std::string enc_args = encode_args(argt, args);
+				ret_msg = enc_type + enc_fname + enc_argt + enc_args;
+				send_result(std::make_pair(send_sockfd, ret_msg));
+			}
+			else {
+				enc_type = std::string("") + (char)EXECUTE_FAILURE;
+				std::string enc_reasonCode = encode_int(result);
+				ret_msg = enc_type + enc_reasonCode;
+				send_result(std::make_pair(send_sockfd, ret_msg));
+			}
+		}
+
+		else if (msg == "TERMINATE") {
+			// also need to verify IP and port 
+			return 0;
+		}
+	}
+	return 0;
+}
